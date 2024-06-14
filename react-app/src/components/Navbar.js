0d0c09628f2c2546958e28fbca9f1a6565c5ec5b
Nishtha Pradhan <nishtha.pradhan@qed42.com>
2024-06-14 13:08:45 +0530

    Fix className in navbar.

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -4,11 +4,11 @@ import logo from '../assets/images/logo.png';
 const Navbar = (props) => {
   return (
     <>
-      <nav class="navbar">
-        <div class="logo">
+      <nav className="navbar">
+        <div className="logo">
             <img src={ logo } alt="React App"></img>
         </div>
-        <ul class="nav-links">
+        <ul className="nav-links">
             <li><a href="/">Home</a></li>
             <li><a href="/blogs">Blogs</a></li>
             <li><a href="/sample-page">Sample Page</a></li>
