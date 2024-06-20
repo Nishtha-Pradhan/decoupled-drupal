d4e11872133ecc3bfbcc697ac06829650466fac2
Nishtha Pradhan <nishtha.pradhan@qed42.com>
2024-06-20 19:14:44 +0530

    WIP blogs updated URL.

--- react-app/src/components/Blogs.js
+++ react-app/src/components/Blogs.js
@@ -8,7 +8,7 @@ function Blogs() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://your-drupal-site.com/api/blogs');
+        const response = await axios.get('https://headless-drupal.ddev.site/api/blogs');
         setViewData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -27,22 +27,22 @@ function Blogs() {
       <h1>Blog Posts</h1>
       {viewData.map((item, index) => (
         <div key={index}>
-        <h2>{item.title}</h2>
-        {item.field_image && item.field_image[0] && (
-          <img
-            src={item.field_image[0].url}
-            alt={item.field_image[0].alt}
-            title={item.field_image[0].title}
-            width={item.field_image[0].width}
-            height={item.field_image[0].height}
-          />
-        )}
-        <div>
-            {item.body && item.body[0] && item.body[0].value 
-              ? `${item.body[0].value.substring(0, 200)}...`
+          <h2>{item.title}</h2>
+          {/* {item.field_image && item.field_image[0] && (
+            <img
+              src={item.field_image[0].url}
+              alt={item.field_image[0].alt}
+              title={item.field_image[0].title}
+              width={item.field_image[0].width}
+              height={item.field_image[0].height}
+            />
+          )}
+          <div>
+            {item.body && item.body[0] && item.body[0].summary 
+              ? `${item.body[0].summary.substring(0, 200)}...`
               : 'No body content available'}
+          </div> */}
         </div>
-      </div>
       ))}
     </div>
   );
